d1d0e24d1f4320fb4a84ea2e58b8410ce2f30d64
Aluno <rafael.zottesso@ifpr.edu.br>
2018-09-26 19:29:22 -0300

    Ajax recebendo uma lista json.

--- aula08_json.php
+++ aula08_json.php
@@ -1,5 +1,29 @@
 {
-	"nome": "Rafael Henrique",
-	"email": "rafael@henrique.com",
-	"fone": "(44) 4444-4444"
+  "pessoas" : [
+  {
+    "id"    : 1,
+    "nome"  : "Rafael Henrique",
+    "email" : "rafael@henrique.com",
+    "fone"  : "(44) 4444-4444"
+  },
+  {
+    "id"    : 2,
+    "nome"  : "Fulano de Tal",
+    "email" : "fulano@ifpr.edu.br",
+    "fone"  : "(44) 9999-9876"
+  },
+  {
+    "id"    : 3,
+    "nome"  : "Ciclano da Silva Sauro",
+    "email" : "ciclano@ifpr.edu.br",
+    "fone"  : "(44) 9999-1234"
+  },
+  {
+    "id"    : 4,
+    "nome"  : "Beltrano Segundo",
+    "email" : "fulano@ifpr.edu.br",
+    "fone"  : "(44) 9999-4444"
+  }
+  ]
+
 }
